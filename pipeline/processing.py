736f6d1fff4732c52e6d14291711cce75e6c2147
rushatdixit <rushat.dixit@gmail.com>
2026-03-11 15:38:27 +0530

    changed to incorporate dam class

--- pipeline/processing.py
+++ pipeline/processing.py
@@ -3,12 +3,12 @@ from sentinelhub import BBox, CRS
 from processing.select_reservoir import select_reservoir_connected_to_dam
 from processing.mask_processing import largest_connected_component
 from pipeline.models import ReservoirResult
+from objects.dam import Dam
 
 def choose_reservoir(
         dam_mask : np.ndarray,
         expanded_dam_bbox : BBox,
-        dam_lat : float,
-        dam_lon : float,
+        dam : Dam,
         resolution : float,
         min_area_km2 : float = 0.01,
         wants_debugs : bool = True
@@ -16,20 +16,18 @@ def choose_reservoir(
 
     assert hasattr(expanded_dam_bbox, 'crs') and expanded_dam_bbox.crs != CRS.WGS84, \
         "choose_reservoir requires UTM bbox"
-    assert isinstance(dam_lat, float)
-    assert isinstance(dam_lon, float)
+    assert isinstance(dam, Dam), "dam must be an instance of Dam class"
     assert dam_mask.ndim == 2, \
         "dam_mask must be 2D"
     assert resolution > 0
 
     selected_mask = select_reservoir_connected_to_dam(
-        dam_mask,
-        dam_lat,
-        dam_lon,
-        expanded_dam_bbox,
-        resolution,
-        min_area_km2,
-        wants_debugs
+        mask=dam_mask,
+        dam=dam,
+        bbox_utm=expanded_dam_bbox,
+        resolution=resolution,
+        min_area_km2=min_area_km2,
+        is_debug=wants_debugs
         )
     selected_contour = largest_connected_component(selected_mask[0])
     water_pixels = np.sum(selected_mask[0])
